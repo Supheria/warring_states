bb15a1f3a996451096d2998fd8c9f1e97a32051c
Supheria <supheria@gmail.com>
2024-05-22 12:33:21 +0800

    prepare to make UI

--- WarringStates/Program.cs
+++ WarringStates/Program.cs
@@ -1,4 +1,4 @@
-#define gen
+//#define gen
 
 using AtlasGenerator;
 using AtlasGenerator.Layout;
@@ -11,14 +11,14 @@ namespace WarringStates
         [STAThread]
         static void Main()
         {
-            var data = new AtlasData("testmap", new(300, 300), new(2, 2), new(6, 6), RiverLayout.Type.ForwardSlash, 2.25, 55000, 0.66f);
-            //var data = new AtlasData("testmap", new(1000, 1000), new(8, 8), new(8, 8), RiverLayout.Type.ForwardSlash, 5, 650000, 0.66f);
+            //var data = new AtlasData("testmap", new(300, 300), new(2, 2), new(6, 6), RiverLayout.Type.ForwardSlash, 2.25, 55000, 0.66f);
+            var data = new AtlasData(new(1000, 1000), new(8, 8), new(8, 8), RiverLayout.Type.ForwardSlash, 7, 650000, 0.75f);
 #if gen
 
             var atlas = new Atlas(data);
             atlas.SaveToSimpleScript(false, "shit_1000.ss");
 #else
-            var atlas = new Atlas("testmap").LoadFromSimpleScript("shit_1000.ss");
+            var atlas = new Atlas().LoadFromSimpleScript("shit_1000.ss");
             atlas.SaveToSimpleScript(false, "shit_1000.ss");
 #endif
             atlas.SetTerrainMap();
