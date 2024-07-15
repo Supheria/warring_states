74e08c2d31c913f1af30ecb8cb8f7ed7c5b64c86
Supheria <Supheria@gmail.com>
2024-07-16 06:46:53 +0800

    use reflection on serialize tool

---
+++ ReflectionSS/RefelctionSerialize/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LocalUtilities.SimpleScript.Common;
+using LocalUtilities.SimpleScript.Parser;
+using LocalUtilities.SimpleScript.Serialization;
+using LocalUtilities.TypeGeneral;
+using LocalUtilities.TypeGeneral.Convert;
+using RefelctionSerialize;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Formats.Asn1;
+using System.Reflection;
+using System.Xml.Linq;
+
+public class A
+{
+    public int Id { get; set; } = 0;
+
+    public string Name { get; set; } = "test";
+
+    public List<int> Ints { get; set; } = [];
+
+    public Dictionary<string, B> Map { get; set; } = [];
+
+    [SsItem(Name = "waht fuck")]
+    public List<B> Bs { get; set; } = [];
+}
+
+public class B
+{
+    public int shit { get; set; } = 0;
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var a = new A()
+        {
+            Id = 1020324,
+            Name = "shitss",
+            Ints = [0, 1, 2, 3],
+            Map = new() { ["1"] = new() { shit = 1 }, ["shit"] = new() { shit = 2 } },
+            Bs = [new(), new()]
+        };
+        var str = a.Serialize(true, null);
+        var st2 = 2.Serialize(true, null);
+        var st3 = new Dictionary<string, List<B>>() { ["raht"] = [new()] }.Serialize(true, "shit");
+        var tokenizer = new Tokenizer(str);
+    }
+}
---
+++ ReflectionSS/RefelctionSerialize/Serialize.cs
@@ -0,0 +1,120 @@
+﻿using LocalUtilities.SimpleScript.Common;
+using LocalUtilities.SimpleScript.Serialization;
+using LocalUtilities.TypeGeneral.Convert;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefelctionSerialize;
+
+public static class SerializeTool
+{
+    public static string Serialize(this object obj, bool writeIntoMultiLines, string? name)
+    {
+        var writer = new SsWriter(writeIntoMultiLines);
+        Serialize(obj, writer, name, true);
+        return writer.ToString();
+    }
+
+    public static void Serialize(object? obj, SsWriter writer, string? name, bool root)
+    {
+        if (obj is null)
+        {
+            writer.AppendValue("");
+            return;
+        }
+        var type = obj.GetType();
+        if (IsSimpleType(type))
+        {
+            writer.AppendValue(obj.ToString() ?? "");
+            return;
+        }
+        if (obj is Point point) 
+        {
+            writer.AppendValue(point.ToArrayString());
+            return;
+        }
+        if (obj is Rectangle rect) 
+        {
+            writer.AppendValue(rect.ToArrayString());
+            return;
+        }
+        if (obj is IArrayStringConvertable iarray) 
+        {
+            writer.AppendValue(iarray.ToArrayString());
+            return;
+        }
+        if (root)
+            writer.AppendName(name ?? type.Name);
+        if (typeof(IDictionary).IsAssignableFrom(type))
+        {
+
+            writer.AppendStart();
+            var enumer = ((IDictionary)obj).GetEnumerator();
+            for (var i = 0; i < ((IDictionary)obj).Count; i++)
+            {
+                enumer.MoveNext();
+                writer.AppendArrayStart(enumer.Key.ToString());
+                if (enumer.Value is null)
+                    continue;
+                Serialize(enumer.Value, writer, null, true);
+                writer.AppendEnd();
+            }
+            writer.AppendEnd();
+            return;
+        }
+        if (typeof(ICollection).IsAssignableFrom(type))
+        {
+            if (IsSimpleTypeCollection(type))
+            {
+                writer.AppendValue(((ICollection)obj).ToArrayString());
+                return;
+            }
+            writer.AppendStart();
+            var enumer = ((ICollection)obj).GetEnumerator();
+            for (var i = 0; i < ((ICollection)obj).Count; i++)
+            {
+                enumer.MoveNext();
+                writer.AppendArrayStart(null);
+                Serialize(enumer.Current, writer, null, true);
+                writer.AppendEnd();
+            }
+            writer.AppendEnd();
+            return;
+        }
+        writer.AppendStart();
+        foreach (var property in obj.GetType().GetProperties())
+        {
+            if (property.GetCustomAttribute<SsIgnore>() is not null)
+                continue;
+            var subObj = property.GetValue(obj);
+            var propertyName = property.GetCustomAttribute<SsItem>()?.Name ?? property.Name;
+            writer.AppendName(propertyName);
+            Serialize(subObj, writer, propertyName, false);
+        }
+        writer.AppendEnd();
+    }
+
+    private static bool IsSimpleType(Type type)
+    {
+        return type == typeof(byte) ||
+            type == typeof(char) ||
+            type == typeof(short) ||
+            type == typeof(int) ||
+            type == typeof(long) ||
+            type == typeof(float) ||
+            type == typeof(double) ||
+            type == typeof(Enum) ||
+            type == typeof(string);
+    }
+
+    private static bool IsSimpleTypeCollection(Type type)
+    {
+        return typeof(ICollection).IsAssignableFrom(type) && type.GenericTypeArguments.Length is 1 && IsSimpleType(type.GenericTypeArguments[0]);
+    }
+}
