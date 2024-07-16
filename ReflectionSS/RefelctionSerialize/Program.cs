9ea05881f1952ad5dc768a4d5044f51b2ea820cc
Supheria <Supheria@gmail.com>
2024-07-17 00:27:51 +0800

    use reflection on deserialize tool

--- ReflectionSS/RefelctionSerialize/Program.cs
+++ ReflectionSS/RefelctionSerialize/Program.cs
@@ -1,17 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LocalUtilities.SimpleScript.Common;
+using LocalUtilities.SimpleScript.Data;
 using LocalUtilities.SimpleScript.Parser;
 using LocalUtilities.SimpleScript.Serialization;
 using LocalUtilities.TypeGeneral;
 using LocalUtilities.TypeGeneral.Convert;
-using RefelctionSerialize;
 using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Formats.Asn1;
 using System.Reflection;
-using System.Xml.Linq;
+using System.Windows.Forms;
+
+namespace RefelctionSerialize;
 
 public class A
 {
@@ -27,9 +26,14 @@ public class A
     public List<B> Bs { get; set; } = [];
 }
 
-public class B
+public class B(int fuck)
 {
-    public int shit { get; set; } = 0;
+    int Fuck { get; set; } = fuck;
+
+    public B() : this(0)
+    {
+
+    }
 }
 
 public class Program
@@ -41,12 +45,109 @@ public class Program
             Id = 1020324,
             Name = "shitss",
             Ints = [0, 1, 2, 3],
-            Map = new() { ["1"] = new() { shit = 1 }, ["shit"] = new() { shit = 2 } },
+            Map = new() { ["1"] = new(1) , ["shit"] = new(2)  },
             Bs = [new(), new()]
         };
         var str = a.Serialize(true, null);
         var st2 = 2.Serialize(true, null);
-        var st3 = new Dictionary<string, List<B>>() { ["raht"] = [new()] }.Serialize(true, "shit");
+        var st3 = new Dictionary<string, List<B>>() { ["raht"] = [new(1024) , new(20022)], ["SSS"] = [new()] }.Serialize(true, "shit");
+        //SerializeTool.Deserialize(a.GetType(), str, null);
+        var st4 = new List<Coordinate>() { new(1, 1), new(2, 1), new(2, 2) }.Serialize(true, "shit");
+        var strt = str.Deserialize<A>(null);
+        var dic = st3.Deserialize<Dictionary<string, List<B>>>("shit");
+        var _4 = st4.Deserialize<List<Coordinate>>("shit");
+    }
+}
+
+partial class SerializeTool
+{
+    public static T? Deserialize<T>(this string str, string? name)
+    {
         var tokenizer = new Tokenizer(str);
+        var type = typeof(T);
+        name ??= type.Name;
+        if (!tokenizer.Elements.Property.TryGetValue(name, out var roots))
+            return default;
+        return (T?)Deserialize(type, roots.LastOrDefault());
+    }
+
+    public static object? Deserialize(Type type, Element? root)
+    {
+        if (root is null)
+            return null;
+        if (GetSimpleTypeConvert(type, out var convert))
+            return convert(root.Value.Text);
+        if (type == TPoint)
+            return root.Value.Text.ToPoint();
+        if (type == TRectangle)
+            return root.Value.Text.ToRectangle();
+        if (typeof(IArrayStringConvertable).IsAssignableFrom(type))
+        {
+            var obj = Activator.CreateInstance(type);
+            if (obj is not IArrayStringConvertable iarr)
+                return null;
+            iarr.ParseArrayString(root.Value.Text);
+            return iarr;
+        }
+        if (root is not ElementScope scope)
+            return null;
+        if (typeof(IDictionary).IsAssignableFrom(type))
+        {
+            var openType = typeof(Dictionary<,>);
+            var pairType = type.GetGenericArguments();
+            var closeType = openType.MakeGenericType(pairType[0], pairType[1]);
+            var obj = Activator.CreateInstance(closeType);
+            if (!GetSimpleTypeConvert(pairType[0], out convert))
+                return obj;
+            foreach (var pair in scope.Property)
+            {
+                var key = convert(pair.Key);
+                var value = Deserialize(pairType[1], pair.Value.FirstOrDefault());
+                var add = type.GetMethod("Add");
+                if (value is not null)
+                    add?.Invoke(obj, [key, value]);
+            }
+            return obj;
+        }
+        if (typeof(ICollection).IsAssignableFrom(type))
+        {
+            var openType = typeof(List<>);
+            var itemType = type.GetGenericArguments()[0];
+            var closeType = openType.MakeGenericType(itemType);
+            var obj = Activator.CreateInstance(closeType);
+            var add = type.GetMethod("Add");
+            if (GetSimpleTypeConvert(itemType, out convert))
+            {
+                var array = scope.Value.Text.ToArray();
+                foreach (var item in array)
+                    add?.Invoke(obj, [convert(item)]);
+            }
+            else if (scope.Property.TryGetValue("", out var items))
+            {
+                foreach (var item in items)
+                {
+                    var itemObj = Deserialize(itemType, item);
+                    if (itemObj is not null)
+                        add?.Invoke(obj, [itemObj]);
+                }
+            }
+            return obj;
+        }
+        else
+        {
+            var obj = Activator.CreateInstance(type);
+            foreach (var property in type.GetProperties(Authority))
+            {
+                if (property.GetCustomAttribute<SsIgnore>() is not null)
+                    continue;
+                var propertyName = property.GetCustomAttribute<SsItem>()?.Name ?? property.Name;
+                if (!scope.Property.TryGetValue(propertyName, out var roots))
+                    continue;
+                var subObj = Deserialize(property.PropertyType, roots.LastOrDefault());
+                if (subObj is not null)
+                    property.SetValue(obj, subObj, Authority, null, null, null);
+            }
+            return obj;
+        }
     }
 }
--- ReflectionSS/RefelctionSerialize/Serialize.cs
+++ ReflectionSS/RefelctionSerialize/Serialize.cs
@@ -4,6 +4,7 @@ using LocalUtilities.TypeGeneral.Convert;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Linq;
 using System.Reflection;
@@ -12,8 +13,21 @@ using System.Threading.Tasks;
 
 namespace RefelctionSerialize;
 
-public static class SerializeTool
+public static partial class SerializeTool
 {
+    public static Type TByte { get; } = typeof(byte);
+    public static Type TChar { get; } = typeof(char);
+    public static Type TShort { get; } = typeof(short);
+    public static Type TInt { get; } = typeof(int);
+    public static Type TLong { get; } = typeof(long);
+    public static Type TFloat { get; } = typeof(float);
+    public static Type TDouble { get; } = typeof(double);
+    public static Type TEnum { get; } = typeof(Enum);
+    public static Type TString { get; } = typeof(string);
+    public static Type TPoint { get; } = typeof(Point);
+    public static Type TRectangle { get; } = typeof(Rectangle);
+
+
     public static string Serialize(this object obj, bool writeIntoMultiLines, string? name)
     {
         var writer = new SsWriter(writeIntoMultiLines);
@@ -34,43 +48,49 @@ public static class SerializeTool
             writer.AppendValue(obj.ToString() ?? "");
             return;
         }
-        if (obj is Point point) 
+        if (type == TPoint)
         {
-            writer.AppendValue(point.ToArrayString());
+            writer.AppendValue(((Point)obj).ToArrayString());
             return;
         }
-        if (obj is Rectangle rect) 
+        if (type == TRectangle) 
         {
-            writer.AppendValue(rect.ToArrayString());
+            writer.AppendValue(((Rectangle)obj).ToArrayString());
             return;
         }
-        if (obj is IArrayStringConvertable iarray) 
+        if (typeof(IArrayStringConvertable).IsAssignableFrom(type)) 
         {
-            writer.AppendValue(iarray.ToArrayString());
+            writer.AppendValue(((IArrayStringConvertable)obj).ToArrayString());
             return;
         }
         if (root)
             writer.AppendName(name ?? type.Name);
         if (typeof(IDictionary).IsAssignableFrom(type))
         {
-
+            var pairType = type.GetGenericArguments();
+            if (!IsSimpleType(pairType[0]))
+            {
+                writer.AppendValue("");
+                return;
+            }
             writer.AppendStart();
             var enumer = ((IDictionary)obj).GetEnumerator();
             for (var i = 0; i < ((IDictionary)obj).Count; i++)
             {
                 enumer.MoveNext();
-                writer.AppendArrayStart(enumer.Key.ToString());
+                //writer.AppendArrayStart(null);
                 if (enumer.Value is null)
                     continue;
-                Serialize(enumer.Value, writer, null, true);
-                writer.AppendEnd();
+                Serialize(enumer.Value, writer, enumer.Key.ToString(), true);
+                //writer.AppendEnd();
             }
             writer.AppendEnd();
             return;
         }
         if (typeof(ICollection).IsAssignableFrom(type))
         {
-            if (IsSimpleTypeCollection(type))
+            var itemType = type.GetGenericArguments()[0];
+            if (IsSimpleType(itemType))
             {
                 writer.AppendValue(((ICollection)obj).ToArrayString());
                 return;
@@ -80,37 +100,71 @@ public static class SerializeTool
             for (var i = 0; i < ((ICollection)obj).Count; i++)
             {
                 enumer.MoveNext();
-                writer.AppendArrayStart(null);
-                Serialize(enumer.Current, writer, null, true);
-                writer.AppendEnd();
+                //writer.AppendArrayStart(null);
+                Serialize(enumer.Current, writer, null, false);
+                //writer.AppendEnd();
             }
             writer.AppendEnd();
             return;
         }
-        writer.AppendStart();
-        foreach (var property in obj.GetType().GetProperties())
+        else
         {
-            if (property.GetCustomAttribute<SsIgnore>() is not null)
-                continue;
-            var subObj = property.GetValue(obj);
-            var propertyName = property.GetCustomAttribute<SsItem>()?.Name ?? property.Name;
-            writer.AppendName(propertyName);
-            Serialize(subObj, writer, propertyName, false);
+            writer.AppendStart();
+            foreach (var property in type.GetProperties(Authority))
+            {
+                if (property.GetCustomAttribute<SsIgnore>() is not null)
+                    continue;
+                var subObj = property.GetValue(obj, Authority, null, null, null);
+                var propertyName = property.GetCustomAttribute<SsItem>()?.Name ?? property.Name;
+                writer.AppendName(propertyName);
+                Serialize(subObj, writer, propertyName, false);
+            }
+            writer.AppendEnd();
         }
-        writer.AppendEnd();
     }
+    
+    static BindingFlags Authority { get; } = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+
 
     private static bool IsSimpleType(Type type)
     {
-        return type == typeof(byte) ||
-            type == typeof(char) ||
-            type == typeof(short) ||
-            type == typeof(int) ||
-            type == typeof(long) ||
-            type == typeof(float) ||
-            type == typeof(double) ||
-            type == typeof(Enum) ||
-            type == typeof(string);
+        return type == TByte ||
+            type == TChar ||
+            type == TShort ||
+            type == TInt ||
+            type == TLong ||
+            type == TFloat ||
+            type == TDouble ||
+            type == TEnum ||
+            type == TString;
+    }
+
+    private static bool GetSimpleTypeConvert(Type type, [NotNullWhen(true)] out Func<string, object?>? convert)
+    {
+        if (type == TByte)
+            convert = str => str.ToByte();
+        else if (type == TChar)
+            convert = str => str.ToChar();
+        else if (type == TShort)
+            convert = str => str.ToShort();
+        else if (type == TInt)
+            convert = str => str.ToInt();
+        else if (type == TLong)
+            convert = str => str.ToLong();
+        else if (type == TFloat)
+            convert = str => str.ToFloat();
+        else if (type == TDouble)
+            convert = str => str.ToDouble();
+        else if (type == TEnum)
+            convert = str => str.ToEnum(type);
+        else if (type == TString)
+            convert = str => str;
+        else
+        {
+            convert = null;
+            return false;
+        }
+        return true;
     }
 
     private static bool IsSimpleTypeCollection(Type type)
