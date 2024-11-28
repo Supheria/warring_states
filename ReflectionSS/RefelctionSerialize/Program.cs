cdae7595980bcdbc3a2f290f2d48d1cbd00624d5
supheria <supheria@gmail.com>
2024-11-28 10:24:46 +0800

    update serialize tool in lib

--- ReflectionSS/RefelctionSerialize/Program.cs
+++ ReflectionSS/RefelctionSerialize/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LocalUtilities.SimpleScript;
+using LocalUtilities.SimpleScript.Common;
 using LocalUtilities.TypeGeneral;
 using System.Drawing;
 using System.Text;
@@ -45,18 +46,21 @@ public class Program
             Map = new() { ["1"] = new(1), ["shit"] = new(2) },
             Bs = [new(), new()]
         };
-        var str1 = SerializeTool.Serialize(a, new(), false, null);
-        var buff1 = SerializeTool.Serialize(a, new(), null, null);
-        var __1 = SerializeTool.Deserialize<A>(new(), buff1, 0, buff1.Length, null, null);
-        var str2 = SerializeTool.Serialize(2, new(), true, null);
-        var str3 = SerializeTool.Serialize(new Dictionary<string, List<B>>() { ["raht"] = [new(1024), new(20022)], ["SSS"] = [new()] }, new("shit"), true, null);
-        var str4 = SerializeTool.Serialize(new List<Coordinate>() { new(1, 1), new(2, 1), new(2, 2) }, new("shit"), true, null);
-        var _1 = SerializeTool.Deserialize<A>(new(), str1, null);
-        var _2 = SerializeTool.Deserialize<int>(new(), str2, null);
-        var _3 = SerializeTool.Deserialize<Dictionary<string, List<B>>>(new("shit"), str3, null);
-        var _4 = SerializeTool.Deserialize<List<Coordinate>>(new("shit"), str4, null);
-        var cc = SerializeTool.Serialize(Color.Aquamarine, new(), true, null);
-        var color = SerializeTool.Deserialize<Color>(new(), cc, null);
+        var signTable = new SsSignTable();
+        //var str1 = SerializeTool.Serialize(a, new(), signTable, false);
+        ////var buff1 = SerializeTool.Serialize(a, new(), signTable, null);
+        ////var __1 = SerializeTool.Deserialize<A>(new(), buff1, 0, buff1.Length, signTable, null);
+        //var str2 = SerializeTool.Serialize(2, new(), signTable, false);
+        //var str3 = SerializeTool.Serialize(new Dictionary<string, List<B>>() { ["raht"] = [new(1024), new(20022)], ["SSS"] = [new()] }, new(), signTable, true);
+        //var str4 = SerializeTool.Serialize(new List<Coordinate>() { new(1, 1), new(2, 1), new(2, 2) }, new("shit"), signTable, true);
+        //var _1 = SerializeTool.Deserialize<A>(new(), str1, signTable);
+        //var _2 = SerializeTool.Deserialize<int>(new(), str2, signTable);
+        //var _3 = SerializeTool.Deserialize<Dictionary<string, List<B>>>(new(), str3, signTable);
+        //var _4 = SerializeTool.Deserialize<List<Coordinate>>(new("shit"), str4, signTable);
+        //var cc = SerializeTool.Serialize(Color.Aquamarine, new(), signTable, false);
+        //var color = SerializeTool.Deserialize<Color>(new(), cc, signTable);
+        var str5 = SerializeTool.Serialize(new Coordinate(101, 202), new(), signTable, false);
+        var site = SerializeTool.Deserialize<Coordinate>(new(), str5, signTable);
         Application.Run(new TestForm());
     }
 }
